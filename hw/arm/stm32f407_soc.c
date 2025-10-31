52592c6afbd90d3a6edc3b5a913a837b8c31f43a
zhouyuming <1006129869@qq.com>
2025-10-31 16:05:14 +0800

    Create stm32f407_soc.c

---
+++ hw/arm/stm32f407_soc.c
@@ -0,0 +1,319 @@
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "exec/address-spaces.h"
+#include "sysemu/sysemu.h"
+#include "hw/arm/stm32f407_soc.h"
+#include "hw/qdev-clock.h"
+#include "hw/misc/unimp.h"
+
+// static const uint32_t timer_addr[STM_NUM_TIMERS] = {
+//     STM32F407_TIM2, STM32F407_TIM3, STM32F407_TIM4,
+//     STM32F407_TIM5
+// };
+
+static const uint32_t usart_addr[STM_NUM_USARTS] = {
+    STM32F407_USART1, STM32F407_USART2, STM32F407_USART3,
+    STM32F407_USART6
+};
+
+// static const uint32_t gpio_addr[STM_NUM_GPIOS] = {
+//     STM_GPIO_PORTA, STM_GPIO_PORTB, STM_GPIO_PORTC, STM_GPIO_PORTD,
+//     STM_GPIO_PORTE, STM_GPIO_PORTF, STM_GPIO_PORTG, STM_GPIO_PORTH,
+//     STM_GPIO_PORTI, STM_GPIO_PORTJ, STM_GPIO_PORTK
+// };
+
+static const int usart_irq[STM_NUM_USARTS] = {
+    37, 38, 39, 71
+};
+
+// static const int exti_irq[] =
+// {
+//     6, 7, 8, 9, 10, 23, 23, 23, 23, 23, 40,
+//     40, 40, 40, 40, 40
+// };
+
+// static const int timer_irq[STM_NUM_TIMERS] = {
+//     28, 29, 30, 50
+// };
+ 
+static void stm32f407_soc_initfn(Object *obj)
+{
+ 
+    STM32F407State *s = STM32F407_SOC(obj);
+    int i;
+ 
+    object_initialize_child(obj, "armv7m", &s->armv7m, TYPE_ARMV7M);
+
+    object_initialize_child(obj, "rcc", &s->rcc, TYPE_STM32_RCC);
+
+    object_initialize_child(obj, "syscfg", &s->syscfg, TYPE_STM32F4XX_SYSCFG);
+
+    for (i = 0; i < STM_NUM_USARTS; i++) {
+        object_initialize_child(obj, "usart[*]", &s->usart[i],
+                                TYPE_STM32F4XX_USART);
+    }
+
+    s->sysclk = qdev_init_clock_in(DEVICE(s), "sysclk", NULL, NULL, 0);
+    s->refclk = qdev_init_clock_in(DEVICE(s), "refclk", NULL, NULL, 0);
+    // sysbus_init_child_obj(obj, "exti", &s->exti, sizeof(s->exti),
+    //                        TYPE_STM32F4XX_EXTI);
+    // sysbus_init_child_obj(obj, "flash", &s->flash, sizeof(s->flash),
+    //                     TYPE_STM32F4XX_FLASH);
+
+    // object_initialize(&s->rcc, sizeof(s->rcc), TYPE_STM32F4XX_RCC);
+    // qdev_set_parent_bus(DEVICE(&s->rcc), sysbus_get_default());
+
+    // object_initialize(&s->power, sizeof(s->power), TYPE_STM32F4XX_POWER);
+    // qdev_set_parent_bus(DEVICE(&s->power), sysbus_get_default());
+
+    // for (i = 0; i < STM_NUM_USARTS; i++) {
+    //     object_initialize(&s->usart[i], sizeof(s->usart[i]), TYPE_STM32F4XX_USART);
+    //     qdev_set_parent_bus(DEVICE(&s->usart[i]), sysbus_get_default());
+    // }
+
+    // for (i = 0; i < STM_NUM_TIMERS; i++) {
+    //     object_initialize(&s->timer[i], sizeof(s->timer[i]), TYPE_STM32F4XX_TIMER);
+    //     qdev_set_parent_bus(DEVICE(&s->timer[i]), sysbus_get_default());
+    // }
+
+    // for (i = 0; i < STM_NUM_GPIOS; i++) {
+    //     object_initialize(&s->gpio[i], sizeof(s->gpio[i]),
+    //                       TYPE_STM32F4XX_GPIO);
+    //     qdev_set_parent_bus(DEVICE(&s->gpio[i]), sysbus_get_default());
+    // }
+}
+ 
+static void stm32f407_soc_realize(DeviceState *dev_soc, Error **errp)
+{
+    STM32F407State *s = STM32F407_SOC(dev_soc);
+    MemoryRegion *system_memory = get_system_memory();
+    DeviceState *dev, *armv7m/*, *touch*/;
+    SysBusDevice *busdev;
+    Error *err = NULL;
+    int i/*, j*/;
+    // // I2CBus *i2c;
+    // // SSIBus *spi_bus;
+
+    /*
+     * We use s->refclk internally and only define it with qdev_init_clock_in()
+     * so it is correctly parented and not leaked on an init/deinit; it is not
+     * intended as an externally exposed clock.
+     */
+    if (clock_has_source(s->refclk)) {
+        error_setg(errp, "refclk clock must not be wired up by the board code");
+        return;
+    }
+
+    if (!clock_has_source(s->sysclk)) {
+        error_setg(errp, "sysclk clock must be wired up by the board code");
+        return;
+    }
+
+    /*
+     * TODO: ideally we should model the SoC RCC and its ability to
+     * change the sysclk frequency and define different sysclk sources.
+     */
+
+    /* The refclk always runs at frequency HCLK / 8 */
+    clock_set_mul_div(s->refclk, 8, 1);
+    clock_set_source(s->refclk, s->sysclk);
+
+    memory_region_init_rom(&s->flash, OBJECT(dev_soc), "STM32F407.flash",
+                           FLASH_SIZE, &err);
+    if (err != NULL) {
+        error_propagate(errp, err);
+        return;
+    }
+    memory_region_init_alias(&s->flash_alias, OBJECT(dev_soc),
+                             "STM32F407.flash.alias", &s->flash, 0,
+                             FLASH_SIZE);
+
+    memory_region_add_subregion(system_memory, FLASH_BASE_ADDRESS, &s->flash);
+    memory_region_add_subregion(system_memory, 0, &s->flash_alias);
+
+    memory_region_init_ram(&s->sram, NULL, "STM32F407.sram", SRAM_SIZE,
+                           &err);
+    if (err != NULL) {
+        error_propagate(errp, err);
+        return;
+    }
+    memory_region_add_subregion(system_memory, SRAM_BASE_ADDRESS, &s->sram);
+
+    memory_region_init_ram(&s->ccm, NULL, "STM32F407.ccm", CCM_SIZE,
+                           &err);
+    if (err != NULL) {
+        error_propagate(errp, err);
+        return;
+    }
+    memory_region_add_subregion(system_memory, CCM_BASE_ADDRESS, &s->ccm);
+ 
+    armv7m = DEVICE(&s->armv7m);
+    qdev_prop_set_uint32(armv7m, "num-irq", 98);
+    qdev_prop_set_uint8(armv7m, "num-prio-bits", 4);
+    qdev_prop_set_string(armv7m, "cpu-type", ARM_CPU_TYPE_NAME("cortex-m4"));
+    qdev_prop_set_bit(armv7m, "enable-bitband", true);
+    qdev_connect_clock_in(armv7m, "cpuclk", s->sysclk);
+    qdev_connect_clock_in(armv7m, "refclk", s->refclk);
+    object_property_set_link(OBJECT(&s->armv7m), "memory",
+                             OBJECT(system_memory), &error_abort);
+    if (!sysbus_realize(SYS_BUS_DEVICE(&s->armv7m), errp)) {
+        return;
+    }
+
+    /* Reset and clock controller */
+    dev = DEVICE(&s->rcc);
+    if (!sysbus_realize(SYS_BUS_DEVICE(&s->rcc), errp)) {
+        return;
+    }
+    busdev = SYS_BUS_DEVICE(dev);
+    sysbus_mmio_map(busdev, 0, RCC_BASE_ADDR);
+
+    /* System configuration controller */
+    dev = DEVICE(&s->syscfg);
+    if (!sysbus_realize(SYS_BUS_DEVICE(&s->syscfg), errp)) {
+        return;
+    }
+    busdev = SYS_BUS_DEVICE(dev);
+    sysbus_mmio_map(busdev, 0, SYSCFG_BASE_ADDR);
+    sysbus_connect_irq(busdev, 0, qdev_get_gpio_in(armv7m, SYSCFG_IRQ));
+
+    /* Attach UART (uses USART registers) and USART controllers */
+    for (i = 0; i < STM_NUM_USARTS; i++) {
+        dev = DEVICE(&(s->usart[i]));
+        qdev_prop_set_chr(dev, "chardev", serial_hd(i));
+        if (!sysbus_realize(SYS_BUS_DEVICE(&s->usart[i]), errp)) {
+            return;
+        }
+        busdev = SYS_BUS_DEVICE(dev);
+        sysbus_mmio_map(busdev, 0, usart_addr[i]);
+        sysbus_connect_irq(busdev, 0, qdev_get_gpio_in(armv7m, usart_irq[i]));
+    }
+
+    // /* EXTI Controller */
+    // dev = DEVICE(&s->exti);
+    // object_property_set_bool(OBJECT(&s->exti), true, "realized", &err);
+    // if (err != NULL) {
+    //     error_propagate(errp, err);
+    //     return;
+    // }
+    // busdev = SYS_BUS_DEVICE(dev);
+    // sysbus_mmio_map(busdev, 0, EXIT_BASE_ADDRESS);
+    // for (i = 0; i < 16; i++) {
+    //     sysbus_connect_irq(busdev, i, qdev_get_gpio_in(armv7m, exti_irq[i]));
+    // }
+    // for (i = 0; i < 16; i++) {
+    //     qdev_connect_gpio_out(DEVICE(&s->syscfg), i, qdev_get_gpio_in(dev, i));
+    // }
+
+    // /* System Power */
+    // dev = DEVICE(&s->power);
+    // object_property_set_bool(OBJECT(&s->power), true, "realized", &err);
+    // if (err != NULL) {
+    //     error_propagate(errp, err);
+    //     return;
+    // }
+    // busdev = SYS_BUS_DEVICE(dev);
+    // sysbus_mmio_map(busdev, 0, POWER_BASE_ADDR);
+
+    // /* GPIO A to K */
+    // for (i = 0; i < STM_NUM_GPIOS; i++) {
+    //     dev = DEVICE(&(s->gpio[i]));
+    //     object_property_set_bool(OBJECT(&s->gpio[i]), true, "realized", &err);
+    //     if (err != NULL) {
+    //         error_propagate(errp, err);
+    //         return;
+    //     }
+    //     busdev = SYS_BUS_DEVICE(dev);
+    //     sysbus_mmio_map(busdev, 0, gpio_addr[i]);
+    // }
+
+    // /* Timer 2 to 5 contoller */
+    // for (i = 0; i < STM_NUM_TIMERS; i++) {
+    //     dev = DEVICE(&(s->timer[i]));
+    //     qdev_prop_set_uint64(dev, "clock-frequency", 1000000000);
+    //     object_property_set_bool(OBJECT(&s->timer[i]), true, "realized", &err);
+    //     if (err != NULL) {
+    //         error_propagate(errp, err);
+    //         return;
+    //     }
+    //     busdev = SYS_BUS_DEVICE(dev);
+    //     sysbus_mmio_map(busdev, 0, timer_addr[i]);
+    //     sysbus_connect_irq(busdev, 0, qdev_get_gpio_in(armv7m, timer_irq[i]));
+    // }
+
+    // /* Flash Controller */
+    // dev = DEVICE(&s->flash);
+    // object_property_set_bool(OBJECT(&s->flash), true, "realized", &err);
+    // if (err != NULL) {
+    //     error_propagate(errp, err);
+    //     return;
+    // }
+    // busdev = SYS_BUS_DEVICE(dev);
+    // sysbus_mmio_map(busdev, 0, FLASH_BASE_ADDR);
+
+    create_unimplemented_device("timer[7]",    0x40001400, 0x400);
+    create_unimplemented_device("timer[12]",   0x40001800, 0x400);
+    create_unimplemented_device("timer[6]",    0x40001000, 0x400);
+    create_unimplemented_device("timer[13]",   0x40001C00, 0x400);
+    create_unimplemented_device("timer[14]",   0x40002000, 0x400);
+    create_unimplemented_device("RTC and BKP", 0x40002800, 0x400);
+    create_unimplemented_device("WWDG",        0x40002C00, 0x400);
+    create_unimplemented_device("IWDG",        0x40003000, 0x400);
+    create_unimplemented_device("I2S2ext",     0x40003000, 0x400);
+    create_unimplemented_device("I2S3ext",     0x40004000, 0x400);
+    create_unimplemented_device("I2C1",        0x40005400, 0x400);
+    create_unimplemented_device("I2C2",        0x40005800, 0x400);
+    create_unimplemented_device("I2C3",        0x40005C00, 0x400);
+    create_unimplemented_device("CAN1",        0x40006400, 0x400);
+    create_unimplemented_device("CAN2",        0x40006800, 0x400);
+    create_unimplemented_device("PWR",         0x40007000, 0x400);
+    create_unimplemented_device("DAC",         0x40007400, 0x400);
+    create_unimplemented_device("timer[1]",    0x40010000, 0x400);
+    create_unimplemented_device("timer[8]",    0x40010400, 0x400);
+    create_unimplemented_device("SDIO",        0x40012C00, 0x400);
+    create_unimplemented_device("timer[9]",    0x40014000, 0x400);
+    create_unimplemented_device("timer[10]",   0x40014400, 0x400);
+    create_unimplemented_device("timer[11]",   0x40014800, 0x400);
+    create_unimplemented_device("GPIOA",       0x40020000, 0x400);
+    create_unimplemented_device("GPIOB",       0x40020400, 0x400);
+    create_unimplemented_device("GPIOC",       0x40020800, 0x400);
+    create_unimplemented_device("GPIOD",       0x40020C00, 0x400);
+    create_unimplemented_device("GPIOE",       0x40021000, 0x400);
+    create_unimplemented_device("GPIOF",       0x40021400, 0x400);
+    create_unimplemented_device("GPIOG",       0x40021800, 0x400);
+    create_unimplemented_device("GPIOH",       0x40021C00, 0x400);
+    create_unimplemented_device("GPIOI",       0x40022000, 0x400);
+    create_unimplemented_device("CRC",         0x40023000, 0x400);
+    create_unimplemented_device("Flash Int",   0x40023C00, 0x400);
+    create_unimplemented_device("BKPSRAM",     0x40024000, 0x400);
+    create_unimplemented_device("DMA1",        0x40026000, 0x400);
+    create_unimplemented_device("DMA2",        0x40026400, 0x400);
+    create_unimplemented_device("Ethernet",    0x40028000, 0x1400);
+    create_unimplemented_device("USB OTG HS",  0x40040000, 0x30000);
+    create_unimplemented_device("USB OTG FS",  0x50000000, 0x31000);
+    create_unimplemented_device("DCMI",        0x50050000, 0x400);
+    create_unimplemented_device("RNG",         0x50060800, 0x400);
+}
+ 
+static void stm32f407_soc_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+ 
+    dc->realize = stm32f407_soc_realize;
+    /* No vmstate or reset required: device has no internal state */
+}
+ 
+static const TypeInfo stm32f407_soc_info = {
+    .name          = TYPE_STM32F407_SOC,
+    .parent        = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(STM32F407State),
+    .instance_init = stm32f407_soc_initfn,
+    .class_init    = stm32f407_soc_class_init,
+};
+ 
+static void stm32f407_soc_types(void)
+{
+    type_register_static(&stm32f407_soc_info);
+}
+ 
+type_init(stm32f407_soc_types)
